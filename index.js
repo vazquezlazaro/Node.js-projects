0b48c5c067208e53846436fe911c407bea5a46b0
Luis <luis.vazquez0651@gmail.com>
2021-04-23 11:50:42 -0400

    JS basic

---
+++ index.js
@@ -0,0 +1,43 @@
+// Refresher to JS to get a job...
+console.log('hello world'); 
+//let name = "Luis";
+//console.log(name);
+
+
+let firstName = 'Luis';
+let lastName = 'Vazquez';
+ console.log('Hello my name is:', firstName, lastName);
+
+const interestRate = 0.3;
+//let age = 27;
+let isApproved = true;
+lastName = null;
+
+const person = {
+    name: 'Luis',
+    age: '27'
+
+};
+console.log(person);
+//changing the name
+person.name = 'Alyssa';
+console.log(person);
+
+
+let selectColors = ['red','blue','green'];
+console.log(selectColors[2]);
+selectColors.push('black');
+console.log(selectColors);
+
+function greet (name) {
+    console.log('Hello ' + name);
+}
+
+greet('Luis');
+
+function square(number) {
+    return number * number;
+}
+
+let number = square(2);
+console.log(number);
